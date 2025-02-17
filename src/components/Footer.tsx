dcd3b04340f45c9f244f74ae39cfbdfd3ed5a4c7
Jonatan_Dev <105404210+jonatanLaureano@users.noreply.github.com>
2025-02-17 16:49:19 -0500

    fix: se corrigio el footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+  <footer className="bg-[#0a0a1a] text-gray-400 py-4 px-6 flex justify-center md:justify-between items-center text-sm">
+      <div className="max-w-4xl w-full mx-auto px-4 flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between text-center md:text-left">
+      <p>&copy; 2025 jonatan. Casi todos los derechos reservados</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
